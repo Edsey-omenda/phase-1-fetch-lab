0efa71441a85e29ddcdc07a1ece6fd9076349abe
Edsey omenda <omendajames0317@gmail.com>
2022-06-14 23:33:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    return response.json();
+  })
+  .then(books => renderBooks(books));
+  //.then(books => books.push(renderBooks(books)))
+  //console.log(fetch)
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
